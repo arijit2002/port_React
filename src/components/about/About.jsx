4ab86eb6cf0b79e92880dcd820973a33149ae6b9
Arijit Das <arijit10das2002@gmail.com>
2022-04-13 12:33:38 +0530

    50:42

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import './about.css'
 import { Container,Row,Col } from 'reactstrap'
 import aboutImg from '../../assets/images/hero__img.png'
+import Education  from './Education'
 
 function About() {
+    const [aboutFilter,setAboutFilter]=useState('ABOUT')
+
   return (
       <section id="about">
           <Container>
@@ -12,19 +15,21 @@ function About() {
 
                   <Col lg='4' md='3'>
                       <div className="about__btns d-flex flex-column align-items-center">
-                          <button className="about__btn about__btn-active">About Me</button>
-                          <button className="about__btn">Education</button>
+                          <button className="about__btn about__btn-active" onClick={()=>setAboutFilter('ABOUT')}>About Me</button>
+                          <button className="about__btn" onClick={()=>setAboutFilter('EDUCATION')}>Education</button>
                           <button className="about__btn">Skills</button>
                           <button className="about__btn">Certificates</button>
                       </div>
                   </Col>
 
                   <Col lg='8' md='9'>
-                      <div className="about__content__wrapper d-flex gap-5">
-                          <div className="about__img w-25">
+                      {
+                          aboutFilter === 'ABOUT' && <div className='about__content__wrapper d-flex gap-5'>
+                              <div className="about__img w-25">
                               <img src={aboutImg} alt="" className='w-100' />
                           </div>
-                          <div className="about__content">
+                            
+                          <div className="about__content w-75">
                               <h2>I'm Arijit Das</h2>
                               <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis itaque quasi impedit alias enim recusandae, excepturi non quis voluptatibus eligendi. Sapiente, itaque repellendus natus cum nihil odit quasi dicta dolorum.</p>
 
@@ -34,7 +39,11 @@ function About() {
                                   <span><a href='https://www.linkedin.com/in/arijit-das-30140620a/' target='_blank' rel="noreferrer"><i class="ri-linkedin-fill"></i></a></span>
                               </div>
                           </div>
-                      </div>
+                          </div>
+                      }
+                      {
+                          aboutFilter === 'EDUCATION' && <Education/>
+                      }
                   </Col>
               </Row>
           </Container>
---
+++ src/components/about/Education.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './education.css'
+
+export const Education = () => {
+  return <table className="tabble">
+      <thead>
+          <tr>
+              <th>Degree</th>
+              <th>Department</th>
+              <th>Institute</th>
+              <th>Year</th>
+          </tr>
+      </thead>
+      <tbody>
+          <tr>
+              <td>BCA</td>
+              <td>Computer Application</td>
+              <td>Institute of Engineering and Management</td>
+              <td>2020-Present</td>
+          </tr>
+          <tr>
+              <td>ISC</td>
+              <td>Computer Science</td>
+              <td>Julien Day School</td>
+              <td>2018-2020</td>
+          </tr>
+          <tr>
+              <td>ICSE</td>
+              <td>Science</td>
+              <td>Julien Day School</td>
+              <td>2016-2018</td>
+          </tr>
+      </tbody>
+  </table>
+}
+
+export default Education
