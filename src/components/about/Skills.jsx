51bba1308ca27f67e96a6172125af7c1e8d656ee
Arijit Das <arijit10das2002@gmail.com>
2022-04-15 22:47:54 +0530

    Update Skills.jsx

--- src/components/about/Skills.jsx
+++ src/components/about/Skills.jsx
@@ -27,13 +27,33 @@ const backendSkills=[
         title: 'Spring Boot'
     },
     {
-        title: 'Oracle SQl'
+        title: 'Oracle SQL'
     },
     {
         title: 'MongoDB'
     },
     {
         title: 'Heroku'
+    }
+];
+const EHSkills=[
+    {
+        title: 'N-Map'
+    },
+    {
+        title: 'Wireshark'
+    },
+    {
+        title: 'Metasploit'
+    },
+    {
+        title: 'WPScan'
+    },
+    {
+        title: 'Burp Suite'
+    },
+    {
+        title: 'Nikto'
     }
 ]
 
@@ -53,6 +73,17 @@ function Skills() {
       <div className="backend__skill w-50">
       {
             backendSkills.map((item,index)=>(
+                <div className="skill__data mb-3">
+                    <div className="skill__title d-flex align-items-center justify-content-between">
+                        <h6>{item.title}</h6>
+                    </div>   
+                </div>
+            ))
+        }
+      </div>
+      <div className="EH__skill w-50">
+      {
+            EHSkills.map((item,index)=>(
                 <div className="skill__data mb-3">
                     <div className="skill__title d-flex align-items-center justify-content-between">
                         <h6>{item.title}</h6>
