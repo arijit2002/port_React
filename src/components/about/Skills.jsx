e3f0ad69ca7e45d319ae8aa76ff63899bd169f7a
Arijit Das <arijit10das2002@gmail.com>
2022-04-15 22:38:47 +0530

    skills adjustments

--- src/components/about/Skills.jsx
+++ src/components/about/Skills.jsx
@@ -4,41 +4,63 @@ import './skills.css'
 
 const frontendSkills=[
     {
-        title: 'JavaScript',
-        width: '60%',
-        percentage: '60%'
+        title: 'HTML'
     },
     {
-        title: 'React',
-        width: '60%',
-        percentage: '60%'
+        title: 'Bootstrap'
     },
     {
-        title: 'JavaScript',
-        width: '60%',
-        percentage: '60%'
+        title: 'CSS'
+    },
+    {
+        title: 'JavaScript'
+    },
+    {
+        title: 'React.js'
+    }
+];
+const backendSkills=[
+    {
+        title: 'Python Flask'
+    },
+    {
+        title: 'Spring Boot'
+    },
+    {
+        title: 'Oracle SQl'
+    },
+    {
+        title: 'MongoDB'
+    },
+    {
+        title: 'Heroku'
     }
 ]
 
 function Skills() {
   return <div className="skills__wrapper d-flex gap-5">
-      <div className="frontend__skill">
+      <div className="frontend__skill w-50">
         {
             frontendSkills.map((item,index)=>(
-                <div className="skill__data">
-                    <div className="skill__title">
+                <div className="skill__data mb-3">
+                    <div className="skill__title d-flex align-items-center justify-content-between">
                         <h6>{item.title}</h6>
-                        <span>{item.percentage}</span>
-                    </div>
-                    <div className="skill__bar">
-                        <span className='skill__bar-percentage'></span>
-                    </div>
+                    </div>   
                 </div>
-
             ))
         }
       </div>
-      <div className="backend__skill"></div>
+      <div className="backend__skill w-50">
+      {
+            backendSkills.map((item,index)=>(
+                <div className="skill__data mb-3">
+                    <div className="skill__title d-flex align-items-center justify-content-between">
+                        <h6>{item.title}</h6>
+                    </div>   
+                </div>
+            ))
+        }
+      </div>
   </div>
 }
 
