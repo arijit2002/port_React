beb7cfc9ed490a4b6ba9fd96ee3d2d2c1d0f017d
Arijit Das <arijit10das2002@gmail.com>
2022-05-29 19:39:48 +0530

    Update Certificates.jsx

--- src/components/about/Certificates.jsx
+++ src/components/about/Certificates.jsx
@@ -7,6 +7,7 @@ export const Certificates = () => {
         <tr>
             <th>Topic</th>
             <th>From</th>
+            <th>Description</th>
             <th>Year</th>
             <th>Link</th>
         </tr>
@@ -15,30 +16,35 @@ export const Certificates = () => {
         <tr>
             <td>Network Security Associate 1 </td>
             <td>Fortinet</td>
+            <td></td>
             <td>2021</td>
             <td><a target="_blank" href='https://drive.google.com/file/d/1e7MSRS7e8tHa5dD0dQwnV0qJjXdyQndi/view?usp=sharing' rel="noreferrer">View</a></td>
         </tr>
         <tr>
             <td>Introduction to Cybersecurity</td>
             <td>CISCO</td>
+            <td></td>
             <td>2021</td>
             <td><a target="_blank" href='https://drive.google.com/file/d/1tAttTpLudSRvOjAyy7iYgK7wx7yutdkp/view?usp=sharing' rel="noreferrer">View</a></td>
         </tr>
         <tr>
             <td>Python Basic Certification</td>
             <td>HackerRank</td>
+            <td></td>
             <td>2021</td>
             <td><a target="_blank" href='https://drive.google.com/file/d/1IzcPyAUQS7HO7i0RAlXyJrYR4tKuI1Yy/view?usp=sharing' rel="noreferrer">View</a></td>
         </tr>
         <tr>
             <td>SQL Basic Certification</td>
             <td>HackerRank</td>
+            <td></td>
             <td>2021</td>
             <td><a target="_blank" href='https://drive.google.com/file/d/1Rfy1bROKX04qLyaOsGo2fenSZSA_2EBM/view?usp=sharing' rel="noreferrer">View</a></td>
         </tr>
         <tr>
             <td>Advent of Cyber 2021</td>
             <td>Try Hack Me</td>
+            <td></td>
             <td>2021</td>
             <td><a target="_blank" href='https://drive.google.com/file/d/1CcNTknTjvDNPwxN72ACuBtJtp6hL8YaW/view?usp=sharing' rel="noreferrer">View</a></td>
         </tr>
