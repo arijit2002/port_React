81097ebb9d8313a0502574b87799c18d5f9dc769
Arijit Das <arijit10das2002@gmail.com>
2022-05-29 19:01:40 +0530

    Update Certificates.jsx

--- src/components/about/Certificates.jsx
+++ src/components/about/Certificates.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 import './certificates.css'
 
 export const Certificates = () => {
-  return (
-    <div>Certificates</div>
-  )
+  return <table className="tabble">
+    <thead>
+        <tr>
+            <th>Degree</th>
+            <th>Department</th>
+            <th>Institute</th>
+            <th>Year</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>BCA</td>
+            <td>Computer Application</td>
+            <td>Institute of Engineering and Management</td>
+            <td>2020-Present</td>
+        </tr>
+        <tr>
+            <td>ISC - 12</td>
+            <td>Computer Science</td>
+            <td>Julien Day School</td>
+            <td>2018-2020</td>
+        </tr>
+        <tr>
+            <td>ICSE - 10</td>
+            <td>General Science</td>
+            <td>Julien Day School</td>
+            <td>2016-2018</td>
+        </tr>
+    </tbody>
+</table>
 }
 
 export default Certificates
