d44449b3b5f27a31cb24d7fd761815fa0f87ef8f
Arijit Das <arijit10das2002@gmail.com>
2022-05-31 08:21:43 +0530

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,12 +2,16 @@ import React, { Fragment } from 'react'
 import About from '../about/About'
 import Header from '../Header/Header'
 import HeroSection from '../hero-section/HeroSection'
+import Portfolio from '../Portfolio/Portfolio'
+import Contact from '../Contact/Contact'
 
 const Layout = () => {
   return <Fragment>
       <Header/>
       <HeroSection/>
       <About/>
+      <Portfolio/>
+      <Contact/>
   </Fragment>
 }
 
