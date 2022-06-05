6413894e392fb664634164cf6f91cf71ba0beac9
Arijit Das <arijit10das2002@gmail.com>
2022-06-05 16:23:52 +0530

    adjustments

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -22,30 +22,33 @@ export const Contact = () => {
       <Container>
         <Row>
           <Col lg='12' className='mb-5'><h2>Contact Me</h2></Col>
-            <Col lg='20'>
-              <form id="form" ref={form} onSubmit={sendEmail}>
-                <div className="field">
-                  <label for="from_name">Enter Your Name Here</label>
-                  <input className='write-name' type='text' name="from_name" required id="from_name"/>
-                </div>
-                <div className="field">
-                  <label for="message">Enter Your Message Here</label>
-                  <input className='write-message' type='text' name="message" required id="message"/>
-                </div>
-                <div className="field">
-                  <label for="reply_to">Enter Your MailID Here</label>
-                  <input className='write-mail' type='email' name="reply_to" required id="reply_to"/>
-                </div>
-                <input className='write' type="submit" id="button" value="Send"/>
-              </form>
-            </Col>
+            <div className='test'>
+              <div className='form-section'>
+                <form id="form" ref={form} onSubmit={sendEmail}>
+                  <div className="field">
+                    <label for="from_name">Enter Your Name Here</label>
+                    <input className='write-name' type='text' name="from_name" required id="from_name"/>
+                  </div>
+                  <div className="field">
+                    <label for="message">Enter Your Message Here</label>
+                    <input className='write-message' type='text' name="message" required id="message"/>
+                  </div>
+                  <div className="field">
+                    <label for="reply_to">Enter Your MailID Here</label>
+                    <input className='write-mail' type='email' name="reply_to" required id="reply_to"/>
+                  </div>
+                  <input className='write' type="submit" id="button" value="Send"/>
+                </form>
+              </div>
               <div className="about__content w-75">
                 <div className="social__links">
-                  <h6 className='mb-3'>Connect with me!!</h6>
+                  <h4 className='mb-3'>Connect with me!!</h4>
                   <span><a href='https://github.com/arijit2002' target='_blank' rel="noreferrer"><i class="ri-github-fill"></i></a></span>
                   <span><a href='https://www.linkedin.com/in/arijit-das-30140620a/' target='_blank' rel="noreferrer"><i class="ri-linkedin-fill"></i></a></span>
                 </div>
               </div>
+            </div>
+             
         </Row>
       </Container>
     </section>
