24f26e83cab58ff55690b5cb8e111945f0fe2c5d
Arijit Das <arijit10das2002@gmail.com>
2022-06-05 09:59:31 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -17,22 +17,24 @@ export const Contact = () => {
   };
 
   return (
-    <form id="form" ref={form} onSubmit={sendEmail}>
-  <div class="field">
-    <label for="from_name">from_name</label>
-    <input type="text" name="from_name" id="from_name"/>
-  </div>
-  <div class="field">
-    <label for="message">message</label>
-    <input type="text" name="message" id="message"/>
-  </div>
-  <div class="field">
-    <label for="reply_to">reply_to</label>
-    <input type="text" name="reply_to" id="reply_to"/>
-  </div>
+    <section className='hero__section' id='nT2gMD=='>
+      <form id="form" ref={form} onSubmit={sendEmail}>
+        <div class="field">
+          <label for="from_name">from_name</label>
+          <input type="text" name="from_name" id="from_name"/>
+        </div>
+        <div class="field">
+          <label for="message">message</label>
+          <input type="text" name="message" id="message"/>
+        </div>
+        <div class="field">
+          <label for="reply_to">reply_to</label>
+          <input type="text" name="reply_to" id="reply_to"/>
+        </div>
 
-  <input type="submit" id="button" value="Send Email"/>
-</form>
+        <input type="submit" id="button" value="Send Email"/>
+      </form>
+    </section>
   );
 };
 
