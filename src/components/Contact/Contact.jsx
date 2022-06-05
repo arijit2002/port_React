2c8fcaa0c1451c3777c2f53d9071b071859cd66c
Arijit Das <arijit10das2002@gmail.com>
2022-06-05 10:27:10 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -23,22 +23,22 @@ export const Contact = () => {
         <h2>Contact Me</h2>
         <Row>
           <Col lg='6' md='6'>
-          <form id="form" ref={form} onSubmit={sendEmail}>
-            <div className="field">
-              <label for="from_name">Enter Your Name Here</label>
-              <input className='write' type="text" name="from_name" id="from_name"/>
-            </div>
-            <div className="field">
-              <label for="message">Enter Your Message Here</label>
-              <input className='write' type="text" name="message" id="message"/>
-            </div>
-            <div className="field">
-              <label for="reply_to">Enter Your MailID Here</label>
-              <input className='write' type="text" name="reply_to" id="reply_to"/>
-            </div>
+            <form id="form" ref={form} onSubmit={sendEmail}>
+              <div className="field">
+                <label for="from_name">Enter Your Name Here</label>
+                <input className='write' type="text" name="from_name" id="from_name"/>
+              </div>
+              <div className="field">
+                <label for="message">Enter Your Message Here</label>
+                <input className='write' type="text" name="message" id="message"/>
+              </div>
+              <div className="field">
+                <label for="reply_to">Enter Your MailID Here</label>
+                <input className='write' type="text" name="reply_to" id="reply_to"/>
+              </div>
 
-            <input className='write' type="submit" id="button" value="Send Email"/>
-          </form>
+              <input className='write' type="submit" id="button" value="Send Email"/>
+            </form>
           </Col>
         </Row>
         
